5774ae79d522da279be345fd50cea280a962195e
Trevlouw <Thelouws6@yahoo.com>
2025-04-30 21:59:25 -0400

    Genesis

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+namespace SnakeControl
+{
+    public class Main : Plugin<Config>
+    {
+        public override string Author => "@Someone";
+        public override string Name => "SnakeControl";
+        public override string Prefix => "SnkCtrl";
+        public static Main Instance { get; private set; }
+        private Harmony Harmony;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            Harmony = new Harmony("SnakeControl");
+            Harmony.PatchAll();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Harmony.UnpatchAll("SnakeControl");
+            Instance = null;
+        }
+    }
+}
---
+++ Patches/ChaosKeycardItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Exiled.API.Features;
+using Exiled.API.Features.Pools;
+using HarmonyLib;
+using InventorySystem.Items.Keycards;
+using InventorySystem.Items.Keycards.Snake;
+using Mirror;
+using UnityEngine;
+using static HarmonyLib.AccessTools;
+namespace SnakeControl.Patches
+{
+    [HarmonyPatch(typeof(ChaosKeycardItem), nameof(ChaosKeycardItem.ServerProcessCustomCmd))]
+    public class ChaosKeycardItemTranspiler
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+            // int index = newInstructions.FindIndex(instruction => instruction.Calls(Constructor(typeof(SnakeNetworkMessage), [typeof(NetworkReader)])))
+            int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ldc_I4_1) - 1;
+            newInstructions.InsertRange(index, 
+                [
+                    new CodeInstruction(OpCodes.Ldloc_1),
+                    new CodeInstruction(OpCodes.Call, Method(typeof(ChaosKeycardItemTranspiler), nameof(Method1))),
+                ]);
+            
+            foreach (CodeInstruction instruction in newInstructions)
+                yield return instruction;
+            
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+        public static void Method1(SnakeNetworkMessage msg)
+        {
+            Log.Warn($"Flags: {msg.Flags.ToString()}");
+            Log.Warn($"Move Offset: {msg.MoveOffset.ToString()}");
+            Log.Warn($"Next Food Position: {msg.NextFoodPosition?.ToString() ?? "NULL"}");
+            string txt = "Segments: ";
+            foreach (Vector2Int segment in msg.Segments)
+            {
+                txt += $"{segment.ToString()}, ";
+            }
+            if (txt.Length > 1) txt = txt.Remove(txt.Length - 2, 2);
+            Log.Warn(txt);
+        }
+    }
+}
